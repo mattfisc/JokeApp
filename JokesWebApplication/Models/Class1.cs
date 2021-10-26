3764f3bb7a1c8cac1191606f5857472972f3fd85
Matthew Fischer <mattfisc@gmail.com>
2021-10-26 08:57:58 -0700

    jokes mvc app

---
+++ JokesWebApplication/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Joke
+{
+	public int Id { get; set; }
+	public String JokeQuestion { get; set; }
+	public String JokeAnswer { get; set; }
+
+	public Joke()
+	{
+		
+	}
+}
